2c7ab8c771b91a2e86c865f5228b1ad689b9ee64
xiaoyiyi567 <sc7767269@gmail.com>
2026-05-06 15:50:23 +0800

    Expose pet size IPC methods

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,5 +1,5 @@
 import { contextBridge, ipcRenderer } from "electron";
-import type { AppSnapshot, PetMode, Settings, SpeechBubble } from "../shared/types";
+import type { AppSnapshot, PetMode, PetSize, Settings, SpeechBubble } from "../shared/types";
 
 type Unsubscribe = () => void;
 
@@ -17,10 +17,15 @@ const api = {
   petDragStop: (): void => ipcRenderer.send("pet:drag-stop"),
   bubbleAction: (actionId: string): void => ipcRenderer.send("bubble:action", actionId),
   updateSettings: (settings: Partial<Settings>): void => ipcRenderer.send("settings:update", settings),
+  setPetSize: (size: PetSize): void => ipcRenderer.send("pet:set-size", size),
+  petSizeSmaller: (): void => ipcRenderer.send("pet:size-smaller"),
+  petSizeLarger: (): void => ipcRenderer.send("pet:size-larger"),
+  resetPetSize: (): void => ipcRenderer.send("pet:size-reset"),
   startFocus: (): void => ipcRenderer.send("focus:start"),
   stopFocus: (): void => ipcRenderer.send("focus:stop"),
   resetToday: (): void => ipcRenderer.send("stats:reset-today"),
   onSnapshot: (callback: (snapshot: AppSnapshot) => void): Unsubscribe => onChannel("app:snapshot", callback),
+  onSettingsUpdated: (callback: (settings: Settings) => void): Unsubscribe => onChannel("settings:updated", callback),
   onPetMode: (callback: (mode: PetMode) => void): Unsubscribe => onChannel("pet:set-mode", callback),
   onShowBubble: (callback: (bubble: SpeechBubble) => void): Unsubscribe => onChannel("pet:show-bubble", callback),
   onHideBubble: (callback: () => void): Unsubscribe => onChannel("pet:hide-bubble", callback)
